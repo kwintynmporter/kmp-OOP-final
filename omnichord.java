9d15439a89133f25eb609e43b593a842a06770f7
kwintynmporter <kwintynmporter@lewisu.edu>
2019-12-01 15:17:59 -0600

    initial commit

--- omnichord.java
+++ omnichord.java
@@ -1,3 +1,10 @@
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import java.net.URL;
+import javax.swing.*;
+
 public class omnichord {
     public static void main(String[] args) {
         System.out.printlnt("Omnichord starting..."); 
